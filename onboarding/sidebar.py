7f6666f5b25292d60b30e059872d2d81aa637ddd
Bora Cukurova <boracukurova@gmail.com>
2024-12-26 20:43:52 +0300

    fixed tr translations

--- onboarding/sidebar.py
+++ onboarding/sidebar.py
@@ -7,6 +7,7 @@ To set Horilla sidebar for onboarding
 from django.contrib.auth.context_processors import PermWrapper
 from django.urls import reverse
 
+from django.utils.translation import gettext_lazy as trans
 from onboarding.templatetags.onboardingfilters import is_taskmanager
 
 MENU = "Onboarding"
@@ -15,12 +16,12 @@ IMG_SRC = "images/ui/rocket.svg"
 
 SUBMENUS = [
     {
-        "menu": "Onboarding view",
+        "menu": trans("Onboarding view"),
         "redirect": reverse("onboarding-view") + "?closed=false",
         "accessibility": "onboarding.sidebar.onboarding_view_accessibility",
     },
     {
-        "menu": "Candidates view",
+        "menu": trans("Candidates view"),
         "redirect": reverse("candidates-view"),
         "accessibility": "onboarding.sidebar.candidates_view_accessibility",
     },
