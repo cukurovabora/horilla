42e4ee6c9e277c1725f0e5522d1b9590ab11ce25
Bora Cukurova <boracukurova@gmail.com>
2025-05-28 09:02:37 +0300

    changed branding

--- base/views.py
+++ base/views.py
@@ -1388,7 +1388,7 @@ def mail_server_test_email(request):
     instance_id = request.GET.get("instance_id")
     white_labelling = getattr(horilla_apps, "WHITE_LABELLING", False)
     image_path = path.join(settings.STATIC_ROOT, "images/ui/horilla-logo.png")
-    company_name = "Horilla"
+    company_name = "Göçtur MyHR"
 
     if white_labelling:
         hq = Company.objects.filter(hq=True).last()
@@ -1410,7 +1410,7 @@ def mail_server_test_email(request):
         form = DynamicMailTestForm(request.POST)
         if form.is_valid():
             email_to = form.cleaned_data["to_email"]
-            subject = _("Test mail from Horilla")
+            subject = _("Test mail from Göçtur MyHR")
 
             # HTML content
             html_content = f"""
